be22cbcdf6d54dcdf84495aeda533b8b4da3c5ed
Dzmitry Shmat <d.shmat.contact@gmail.com>
2025-05-20 16:11:17 +0400

    feat: app setup, TailwindCSS setup, add header, add footer

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'tp-header',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
