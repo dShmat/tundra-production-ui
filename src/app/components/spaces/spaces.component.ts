3b1f3be491d5d7e7398daed43c61657c37e5b386
Dzmitry Shmat <d.shmat.contact@gmail.com>
2025-06-15 00:03:11 +0400

    feat: add different spaces options

---
+++ src/app/components/spaces/spaces.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tp-spaces',
+  imports: [],
+  templateUrl: './spaces.component.html',
+  styleUrl: './spaces.component.scss'
+})
+export class SpacesComponent {
+
+}
